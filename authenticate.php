69fb94b368b4b0c28f58990479880c54a05571d2
Panic04 <nicoladimartinorusso3@gmail.com>
2022-11-12 18:00:01 +0100

    6

--- authenticate.php
+++ authenticate.php
@@ -1,50 +1,32 @@
 <?php
-session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'u746375557_admin';
-$DATABASE_PASS = 'Panic12345.';
-$DATABASE_NAME = 'u746375557_phplogin';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
-}
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-  if ($stmt->num_rows > 0) {
-	$stmt->bind_result($id, $password);
-	$stmt->fetch();
-	// Account exists, now we verify the password.
-	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-  if ($_POST['password'] === $password) {
-		// Verification success! User has logged-in!
-		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-		session_regenerate_id();
-		$_SESSION['loggedin'] = TRUE;
-		$_SESSION['name'] = $_POST['username'];
-		$_SESSION['id'] = $id;
-		header('Location: home.php');
-	} else {
-		// Incorrect password
-		echo 'Incorrect username and/or password!';
-	}
-} else {
-	// Incorrect username
-	echo 'Incorrect username and/or password!';
-}
+        include('connection.php');
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
 
-	$stmt->close();
-}
-?>
+            //to prevent from mysqli injection
+            $username = stripcslashes($username);
+            $password = stripcslashes($password);
+            $username = mysqli_real_escape_string($con, $username);
+            $password = mysqli_real_escape_string($con, $password);
+
+            $sql = "select *from accounts where username = '$username' and password = '$password'";
+            $result = mysqli_query($con, $sql);
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $count = mysqli_num_rows($result);
+
+            if($count == 1){
+                echo "<h1><center> Login successful </center></h1>";
+                session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+
+                            // Redirect user to welcome page
+                            header("location: welcome.html");
+            }
+            else{
+                echo "<h1> Login failed. Invalid username or password.</h1>";
+            }
+    ?>
---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+    $host = "localhost";
+    $user = "pmauser";
+    $password = 'Panic12345.';
+    $db_name = "phplogin";
+
+    $con = mysqli_connect($host, $user, $password, $db_name);
+    if(mysqli_connect_errno()) {
+        die("Failed to connect with MySQL: ". mysqli_connect_error());
+    }
+?>
